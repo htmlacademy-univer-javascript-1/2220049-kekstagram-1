11bfee4411a398bcfd258f40fb11f1dde4c267e9
KazantsevaJane <103194481+KazantsevaJane@users.noreply.github.com>
2022-12-12 10:24:46 +0500

    Исправление ошибок 2

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,4 +1,4 @@
-import { showBigPicture } from './bigpicture.js';
+import { showBigPicture } from './big-picture.js';
 const pictures = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture')
   .content
